7f47d0622ed0a8654e14f5f8fa64925f8af2cb42
Sylvestre <ibombogakossosylvestre@gmail.com>
2025-06-24 23:15:46 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > window.innerHeight-30) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > window.innerHeight-30) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
